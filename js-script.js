352dc90849c0215229065c7dd7f9c8e654babe52
Lera <ohhsith@ukr.net>
2021-09-22 09:27:34 +0300

    fixes for 1,3,4

--- js-script.js
+++ js-script.js
@@ -5,24 +5,15 @@ let input = document.getElementById('title');
 async function getTodos() {
     let todos = await fetch('http://localhost:3000/todos')
     let todosArray = await todos.json();
-
     todoList.innerHTML = todosArray.map(function (obj) {
-        // if (obj.completed === false) {
-        //     return `<li class="in-progress" data-id="${obj.id}" data-title="${obj.title}" data-complited="${obj.completed}">${obj.title}<button type="submit" id="delete">delete</button></li>`
-        // } else {
-        //     return `<li class="confirmed" data-id="${obj.id}" data-title="${obj.title}" data-complited="${obj.completed}">${obj.title}<button type="submit" id="delete">delete</button></li>`
-        // }
         let status = obj.completed === false ? "in-progress" : "confirmed"
         return `<li class="${status}" data-id="${obj.id}" data-title="${obj.title}" data-complited="${obj.completed}">${obj.title}<button type="submit" id="delete">delete</button></li>`
 
     }).join('');
 
 
-
 };
-getTodos();
-
-
+getTodos()
 
 create.addEventListener('click', async function addTodo(e) {
     e.preventDefault()
@@ -37,15 +28,9 @@ create.addEventListener('click', async function addTodo(e) {
             "completed": false
         })
     });
-
-
     let newTodoArray = await newTodo.json();
-    console.log(newTodoArray)
-    // todoList.innerHTML += `<li class="in-progress" data-id="${newTodoArray.id}" data-title="${newTodoArray.title}"  
-    // data-complited="${newTodoArray.completed = false}">${newTodoArray.title}<button type="submit" id="delete">delete</button></li>`;
+    todoList.innerHTML += `<li class="in-progress"  data-title="${input.value}" data-complited="false">${input.value}<button type="submit" id="delete">delete</button></li>`
     input.value = ' '
-    getTodos();
-
 })
 
 todoList.addEventListener('click', async function changeTodoList(e) {
@@ -65,18 +50,17 @@ todoList.addEventListener('click', async function changeTodoList(e) {
 
         let changedToDo = await changeToDo.json();
         e.target.classList.add('confirmed')
-        // console.log(changedToDo)
+
     }
 })
 
-// let del = document.getElementById('delete')
+
 todoList.addEventListener('click', async function deleteTodo(e) {
     if (e.target.tagName === 'BUTTON') {
         let elemId = e.target.closest('li').getAttribute('data-id')
         let deleteTodo = await fetch(`http://localhost:3000/todos/${elemId}`, {
             method: 'DELETE'
         });
-
+        e.target.closest('li').remove()
     }
-    getTodos();
 })
