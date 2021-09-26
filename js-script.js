46f5d02de6fa71efe5f1573e6cf653882d9d27b3
Lera <ohhsith@ukr.net>
2021-09-26 14:46:56 +0300

    1st

--- js-script.js
+++ js-script.js
@@ -1,66 +1,74 @@
-let todoList = document.querySelector('.to-do-list');
-let create = document.getElementById('create');
-let input = document.getElementById('title');
+let $todoList = $('.to-do-list');
+let $create = $('#create');
+let $input = $('#title');
 
 async function getTodos() {
-    let todos = await fetch('http://localhost:3000/todos')
-    let todosArray = await todos.json();
-    todoList.innerHTML = todosArray.map(function (obj) {
-        let status = obj.completed === false ? "in-progress" : "confirmed"
-        return `<li class="${status}" data-id="${obj.id}" data-title="${obj.title}" data-complited="${obj.completed}">${obj.title}<button type="submit" id="delete">delete</button></li>`
-
-    }).join('');
-
-
-};
-getTodos()
-
-create.addEventListener('click', async function addTodo(e) {
-    e.preventDefault()
-
-    let newTodo = await fetch('http://localhost:3000/todos', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify({
-            "title": input.value,
-            "completed": false
-        })
+    let todos = $.ajax({
+        url: 'http://localhost:3000/todos',
+        dataType: "json",
+        success: function (data) {
+            return data;
+        }
     });
-    let newTodoArray = await newTodo.json();
-    todoList.innerHTML += `<li class="in-progress"  data-title="${input.value}" data-complited="false">${input.value}<button type="submit" id="delete">delete</button></li>`
-    input.value = ' '
-})
+    return todos;
+}
 
-todoList.addEventListener('click', async function changeTodoList(e) {
-    if (e.target.tagName === 'LI') {
-        let elemId = e.target.getAttribute('data-id')
-        let elemTitle = e.target.getAttribute('data-title')
-        let changeToDo = await fetch(`http://localhost:3000/todos/${elemId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify({
-                "title": `${elemTitle}`,
-                "completed": true
-            })
+function render(dataArray) {
+    let listView = '';
+    $.each(dataArray, function (obj, value) {
+        let status = value.completed === 'false' ? "in-progress" : "confirmed"
+        listView += ` <li class="${status}" data-id="${value.id}" data-title="${value.title}" data-complited="${value.completed}"><span>${value.title}</span><button type="submit" id="delete">delete</button></li> `
+    })
+    $todoList.html(listView)
+}
+
+async function getTodosAndRender() {
+    const data = await getTodos();
+    render(data);
+};
+getTodosAndRender()
+
+$create.on('click',async function addTodo(e) {
+    
+    e.preventDefault();
+      
+    $.post( 'http://localhost:3000/todos', { "title": $input.val(), "completed": false})
+        .done(function( data ) {
+            $todoList.append(`<li class="in-progress" data-title="${$input.val()}" data-complited="false"><span>${$input.val()}</span><button type="submit" id="delete">delete</button></li>`)
+            $input.val(' ')
         });
 
-        let changedToDo = await changeToDo.json();
-        e.target.classList.add('confirmed')
+})
 
-    }
+$todoList.on('click', 'span', async function changeTodoList() {
+        let $elemId = $(this).parent().attr('data-id');
+        let $elemTitle = $(this).parent().attr('data-title');
+        console.log($elemId)
+
+
+        $.ajax({
+            url: 'http://localhost:3000/todos/'+$elemId,
+            type: 'PUT',
+            data: {
+                        "title": $elemTitle,
+                        "completed": true
+                    },
+         });
+
+        $(this).addClass('confirmed')
+
 })
 
 
-todoList.addEventListener('click', async function deleteTodo(e) {
-    if (e.target.tagName === 'BUTTON') {
-        let elemId = e.target.closest('li').getAttribute('data-id')
-        let deleteTodo = await fetch(`http://localhost:3000/todos/${elemId}`, {
-            method: 'DELETE'
-        });
-        e.target.closest('li').remove()
-    }
+$todoList.on('click', 'button', async function deleteTodo(e) {
+    
+    let $elemId = $(this).parent().attr('data-id');
+    console.log($elemId)
+    $.ajax({
+        url: 'http://localhost:3000/todos/'+$elemId,
+        type: 'DELETE'
+        
+     });
+     $(this).parent().remove()
+
 })
